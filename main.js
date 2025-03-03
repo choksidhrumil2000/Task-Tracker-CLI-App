713a4afcea72b4ec6c1020b82c5c74d032f5dc08
choksidhrumil2000 <choksidhrumil2000@gmail.com>
2025-03-03 13:32:29 +0530

    Add Feature Added

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-let data = [
-
-];
+const readline = require('readline');
+const data = require('./myData.js');
 
 const STATUS = {
     "TODO":"TODO",
@@ -8,4 +7,96 @@ const STATUS = {
     "DONE":"DONE"
 }
 
+const COMMANDS = [
+    "add",
+    "update",
+    "delete",
+    "mark-in-progress",
+    "mark-done",
+    "list",
+];
 
+let id = 1;
+let command = "exit";
+let args = [];
+let askAgain = false;
+
+// const arguments = process.argv;
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// rl.question('Command: ', (input) => {
+//     if(isCommandValid(input)){
+//         command = input;
+//     }else{
+//         command = "invalid command";
+//     }
+//     rl.close();
+// });
+// while(command !== "exit"){
+        
+//         rl.question('Command: ', (input) => {
+//             if(isCommandValid(input)){
+//                 command = input;
+//             }else{
+//                 command = "invalid Command";   
+//             }
+//         rl.close();
+//         });   
+
+// }
+
+(function loop() {
+  rl.question("command: ", (cmd) => {
+    const isValid = isCommandValid(cmd);
+    if(isValid){
+        if(cmd !== 'exit'){
+            askAgain = true;
+        }else{
+            askAgain = false;
+            rl.close()
+            return;
+        }
+
+        switch(args[1]){
+            case "add": add(args[2]);break;
+        }
+        if (askAgain) loop();
+        // else rl.close();
+    }
+  });
+})();
+
+
+function isCommandValid(cmd) {
+    args = cmd.split(" ");
+    if(args[0] !== "exit" && args[0] !== "task-cli" ||(args[1] && !COMMANDS.includes(args[1]))){
+        console.log("invalid Command!!");
+        return false;
+    }
+    return true;
+}
+
+
+
+function getId(){
+    return id;
+}
+
+function add(item) {
+    let obj = {
+        id:getId(),
+        description:item,
+        status:STATUS.TODO,
+        createdAt:new Date(),
+        updatedAt:new Date(),
+    }
+    
+    data.push(obj);
+    console.log(`Task Added Successfully ID:${id}`);
+    console.log(data);
+    id++;   
+}
